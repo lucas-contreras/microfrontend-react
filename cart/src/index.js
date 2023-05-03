7b12f230a9e9669a984d769db937af7c38a1bc9a
Lucas Contreras <cont.lucas@gmail.com>
2023-05-02 22:59:28 -0300

    chore: added a shared module federation

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from 'faker';
-
-const cartText = `<div>you have ${faker.random.number()} items in your cart</div>`;
-
-document.querySelector('#dev-carts').innerHTML = cartText;
+import('./bootstrap')
